1e1a8274dde8afad2312fb0814ff10366610ed59
yourname <youremail>
2017-11-17 00:09:30 +0800

    登录功能未完成

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,20 @@
 
-	NProgress.start();
-
-	NProgress.done();
-
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	$("#loginForm").on('submit',(function() {
+      var formData = $(this).serialize();
+      $.ajax({     
+		url: "/api/login",
+		type: "post",
+        data: formData,
+        dataType: "json",
+        success: function(data) {
+			if(data.code === 200){
+				location.href = "/";
+			}       
+        }
+      });
+      return false;
+    }));
